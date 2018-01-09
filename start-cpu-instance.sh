8c7cfb7a629ba539ec5242f382b19dd4e4388378
Robert Lowry <robert@nsfworks.com>
2018-01-08 22:32:00 -0600

    added defaults to start-*-instance.sh

--- start-cpu-instance.sh
+++ start-cpu-instance.sh
@@ -1,7 +1,12 @@
 INSTANCE_NAME=$1
+IMAGE_NAME=$2
+
+if [ -z "${INSTANCE_NAME}" ]; then
+	INSTANCE_NAME='spawn'
+fi
 
 if [ -z "${IMAGE_NAME}" ]; then
-	IMAGE_NAME=cpu-spawn
+	IMAGE_NAME='cpu-base'
 fi
 
 MACHINE_TYPE='n1-standard-4'
@@ -9,12 +14,12 @@ ZONE='us-central1-c'
 DISK_SIZE=64GB
 
 echo Creating instance $1
-gcloud beta compute instances create $1 \
-	--machine-type=${MACHINE_TYPE} --image $2 --zone=${ZONE} \
+gcloud compute instances create ${INSTANCE_NAME} \
+	--machine-type=${MACHINE_TYPE} --image ${IMAGE_NAME} --zone=${ZONE} \
 	--boot-disk-size=${DISK_SIZE} \
 	--preemptible --maintenance-policy=TERMINATE
 
-echo -n 'Waiting for instance '
-until gcloud compute --verbosity error ssh $1 -- echo ' instance started'; do
+echo 'Waiting for instance ...'
+until gcloud compute --verbosity error ssh ${INSTANCE_NAME} -- echo ' instance started'; do
 	echo -n .
 done
--- start-gpu-instance.sh
+++ start-gpu-instance.sh
@@ -1,16 +1,23 @@
-IMAGE_NAME=$1
+INSTANCE_NAME=$1
+IMAGE_NAME=$2
+
+if [ -z "${INSTANCE_NAME}" ]; then
+	INSTANCE_NAME='spawn'
+fi
 
 if [ -z "${IMAGE_NAME}" ]; then
-	IMAGE_NAME='gpu-spawn'
+	IMAGE_NAME='gpu-base'
 fi
 
 MACHINE_TYPE='n1-standard-4'
 ACCELERATOR='type=nvidia-tesla-k80,count=1'
 ZONE='us-central1-c'
+DISK_SIZE=64GB
 
-echo Creating instance $1
-gcloud beta compute instances create $1 --accelerator=${ACCELERATOR} \
-	--machine-type=${MACHINE_TYPE} --image $2 --zone=${ZONE} \
+echo Creating instance ${INSTANCE_NAME} ...
+gcloud beta compute instances create ${INSTANCE_NAME} --accelerator=${ACCELERATOR} \
+	--machine-type=${MACHINE_TYPE} --image ${IMAGE_NAME} --zone=${ZONE} \
+	--boot-disk-size=${DISK_SIZE} \
 	--preemtible --maintenance-policy=TERMINATE
 
 echo 'Waiting for instance ...'
